2b7ae1f3faac0bce62d794145c61f640d438efca
Md Najamuddin <786mdnajamuddin@gmail.com>
2024-12-29 12:06:43 +0530

    EMAIL

--- Frontend/src/components/ContactUs/ConMain.jsx
+++ Frontend/src/components/ContactUs/ConMain.jsx
@@ -1,8 +1,46 @@
 import { Box,Flex,HStack,Input,Text,Button, Textarea, VStack, Center,Image } from "@chakra-ui/react";
+import { useState } from "react";
+import emailjs from '@emailjs/browser';
 
 
+
+function Send(){
+    return <Flex borderRadius={'500px'}  w={{base:"100%",md:"48%",lg:"45%",xl:'33%'}} gap='8' bg='white'  flexDirection={'column'}>
+    
+            <Center><Button  w='100%'>Massage Sending Completed</Button></Center>
+</Flex>
+}
 
 export default function ConMain(){
+
+    let [data,setData]=useState({name:"",number:"",massage:""})
+    let [send,setSend]=useState(false)
+
+    function setDa(){
+        if(data.name==false){
+            alert('Please Enter Your Name')
+
+        }else if(data.number==false){
+            alert('Please Enter Your Number')
+
+        }else if(data.massage==false){
+            alert('Please Enter Your Massage')
+        }
+        if(data.name && data.number && data.massage){
+            console.log(data)
+            emailjs.init('1az4QfowAhgDDgLMV');
+
+            emailjs.send('service_reo0aru', 'template_bgvbsfc', data)
+                .then(() => {
+                    setSend(true)
+                })
+                .catch((error) => {
+                    console.error('Failed to send email:', error);
+                });
+
+        }
+
+    }
 
     return <>
            <Flex gap='10' px={'5vw'} py='10' bg='blue.100' flexDirection={'column'}>
@@ -10,24 +48,28 @@ export default function ConMain(){
                     <Box fontFamily={'cursive'} color={'white'} textAlign={'center'}  borderRadius={'20px 2px'} bg='red' w='50px' >CU</Box>
                     <Box className="cssanimation lePeek sequence cssanimation effect3d">Contact Us</Box>
                 </Flex>
-                <HStack gap='10' flexDirection={{base:'column',md:"row"}} w='100%' justifyContent={'space-between'}>
+                <HStack  gap='10' flexDirection={{base:'column',md:"row"}} w='100%' justifyContent={'space-between'}>
 
                     {/* ----------------Contect Us */}
-                    <Flex borderRadius={'md'} w={{base:"100%",md:"48%",lg:"45%",xl:'33%'}} gap='8' bg='white' px={{base:"3",sm:"10"}} py='20' flexDirection={'column'}>
+
+                   
+                   {send ? <Send/> :''}
+                   {send==false ?<Flex borderRadius={'md'} w={{base:"100%",md:"48%",lg:"45%",xl:'33%'}} gap='8' bg='white' px={{base:"3",sm:"10"}} py='20' flexDirection={'column'}>
                         <VStack alignItems={'left'} gap='0'>
                             <HStack fontWeight={'600'} alignItems={'center'}><Text>Name</Text><Text color={'red'}>*</Text></HStack>
-                            <Input placeholder="Enter Your Name"/>
+                            <Input placeholder="Enter Your Name" required onChange={(e)=>{setData({...data,name:e.target.value})}}/>
                         </VStack>
                         <VStack alignItems={'left'} gap='0'>
                             <HStack fontWeight={'600'} alignItems={'center'}><Text>Number</Text><Text color={'red'}>*</Text></HStack>
-                            <Input placeholder="Enter Your Number"/>
+                            <Input placeholder="Enter Your Number" required onChange={(e)=>{setData({...data,number:e.target.value})}}/>
                         </VStack>
                         <VStack alignItems={'left'} gap='0'>
-                            <HStack fontWeight={'600'} alignItems={'center'}><Text>Number</Text><Text color={'red'}>*</Text></HStack>
-                            <Textarea placeholder="Enter Your Number"/>
+                            <HStack fontWeight={'600'} alignItems={'center'}><Text>Massage</Text><Text color={'red'}>*</Text></HStack>
+                            <Textarea placeholder="Enter Your Massage" required onChange={(e)=>{setData({...data,massage:e.target.value})}}/>
                         </VStack>
-                       <Center><Button w='100%'>Submit</Button></Center>
-                    </Flex>
+                       <Center><Button onClick={setDa} w='100%'>Submit</Button></Center>
+                    </Flex> :""}
+                    
 
 
 {/* ------------------Sidebar */}
@@ -56,7 +98,7 @@ export default function ConMain(){
                         </Flex>
                        </Flex>
                        <Box  h='350px' w='full'>
-                            <Center><Image borderRadius={'500px'} h='350px' w='350px' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgTc2V1FngTfLoX526H97RWBP68sOQE7BJ6Q&s'/>
+                            <Center><Image boxShadow={'2xl'} borderRadius={'500px'} h='350px' w='350px' src='https://i.pinimg.com/originals/1d/90/a7/1d90a79861d29d072350c01901b2828f.jpg'/>
                             </Center>
                         </Box>
 
